d47bfff92dd49301f9992814befa6634f27d2764
Abhilash <abhilash.27n@gmail.com>
2016-03-01 18:30:20 -0500

    Simple working app requesting data from google finance. Used ajax for request and response processing

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,26 @@
 var express = require('express');
+var googleStocks = require('google-stocks');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
-router.get('/', function(req, res){
-	res.render('index', { title: 'JSON Object' })
-	//res.send('USE FOR API');
-})
+router.post('/', function(req, res){
+
+	var stockName = req.body.stock;
+	console.log("REQUESTING FOR: "+stockName);
+
+	googleStocks.get([stockName], function(error, data) {
+	  console.log("RECEIVED RESPONSE");
+	  res.send(JSON.stringify(data));
+	});
+
+
+	//res.render('index', { title: stockName });
+	//res.send('USE FOR API OR HTTP STATUS OR DIRECT JSON OBJECT');
+	//req.query.param
+});
 
 module.exports = router;
